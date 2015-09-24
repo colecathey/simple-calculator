e7126f6cfd2ad0b68a96e2028f3015d034d4f8c3
colecathey <cole.cathey@outlook.com>
2015-09-23 19:19:08 -0500

    initial push

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            string operand;
+            float answer;
+
+            Console.WriteLine("What is your 1st number?:\n");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What operation are you trying to perform? (+,-,*,/,%)\n");
+            operand = Console.ReadLine();
+
+            Console.WriteLine("What is your 2nd number?:\n");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (operand)
+            {
+                case "+":
+                    answer = num1 + num2;
+                    break;
+                case "-":
+                    answer = num1 - num2;
+                    break;
+                case "*":
+                    answer = num1 * num2;
+                    break;
+                case "/":
+                    answer = num1 / num2;
+                    break;
+                case "%":
+                    answer = num1 % num2;
+                    break;
+                default:
+                    answer = 0;
+                    break;
+            }
+
+            Console.WriteLine("Your answer is " + answer.ToString());
+            Console.ReadLine();
+        }
+    }
+}
