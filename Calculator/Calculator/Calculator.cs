15024eaa054d1f01df91e94adfd96e86ee1fc15d
colecathey <cole.cathey@outlook.com>
2015-09-24 21:06:35 -0500

    end of night push
    got counter working... maybe. got split working... maybe

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -7,35 +7,22 @@ using System.Threading.Tasks;
 namespace Calculator
 {
     
-    class Program : Operations
+    class Program : Slicer
     {
         static void Main(string[] args)
         {
-            decimal num1;
-            decimal num2;
-            string operand;
-            decimal answer;
+            int lineCount = 0;
+            string userInput;
+            Console.WriteLine(lineCount + ">");
+            userInput = Console.ReadLine();
 
-            Console.WriteLine("What is your 1st number?:\n");
-            num1 = Convert.ToDecimal(Console.ReadLine());
-
-            Console.WriteLine("What operation are you trying to perform? (+,-,*,/,%)\n");
-            operand = Console.ReadLine();
-
-            Console.WriteLine("What is your 2nd number?:\n");
-            num2 = Convert.ToDecimal(Console.ReadLine());
-
-            if (operand == "+")
-            {
-                answer = add(num1, num2);               
-            } else if (operand == "-")
-            {
-                answer = subtract(num1, num2);
-            }
 
             
-            Console.WriteLine("Your answer is " + answer.ToString());
-            Console.ReadLine();
+
+
+
+            lineCount++;
+
         }
     }
 }
---
+++ Calculator/Calculator/Slicer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Slicer
+    {
+        public string inputSlice()
+        {
+            int num1;
+            string opperand;
+            int num2;
+            string userInput = Console.ReadLine();
+     
+            string[] slicedInputs = userInput.Split();
+
+            for(var i = 0; i < slicedInputs.Length; i++)
+            {
+                
+
+                
+                    
+                
+            }
+
+            //string userInput = Console.ReadLine();
+            //char[] numInput = userInput.Split(delimiterChars);
+
+            
+
+            return numInput;
+        }
+    }
+}
