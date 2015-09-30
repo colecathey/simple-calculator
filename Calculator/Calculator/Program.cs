948a0146a7a296b57420b12d4790a163cd7020ef
colecathey <cole.cathey@outlook.com>
2015-09-30 17:54:14 -0500

    worked on slicer

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    
+    class Program : Slicer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter ze Maths");
+            Console.WriteLine();
+            Console.ReadLine();
+            //int lineCount = 0;
+            //string userInput;
+            //Console.WriteLine(lineCount + ">");
+            //Console.ReadLine();
+            //userInput = Console.ReadLine();            
+
+        }
+    }
+}
--- Calculator/Calculator/Slicer.cs
+++ Calculator/Calculator/Slicer.cs
@@ -10,36 +10,62 @@ namespace Calculator
     {
         public string inputSlice()
         {
-            //int num1;
-            //string operand;
-            //int num2;
+            int num1;
+            string operand;
+            int num2;
 
             //get user input
             string userInput = Console.ReadLine();
 
             //split user input into string array
-            string[] slicedInputs = userInput.Split();
+            string[] allSlicedInputs = userInput.Split();
 
             //define delimiters to get the numbers inputed and the operands
             char[] numSlice = { '+', '-', '*', '/', '%' };
-            char[] operandSlice = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            //char[] operandSlice = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            foreach(string j in allSlicedInputs)
+            {
+                if (j == "+")
+                {
+                    operand = j;
+                }
+                else if(j == "-")
+                {
+                    operand = j;
+                }
+                else if (j == "*")
+                {
+                    operand = j;
+                }
+                else if (j == "/")
+                {
+                    operand = j;
+                }
+                else if (j == "%")
+                {
+                    operand = j;
+                } else
+                {
+                    break;
+                }
+            }
+
 
             //returns 2 elements seperated by operands hopefully. this will get num inputs
-            string[] numInput = userInput.Split(numSlice,2, System.StringSplitOptions.RemoveEmptyEntries);
+            string[] numInput = userInput.Split(numSlice,System.StringSplitOptions.RemoveEmptyEntries);
+
+            num1 = int.Parse(numInput[0]);
+            num2 = int.Parse(numInput[1]);
 
             //returns operand from user input
-            string[] operandInput = userInput.Split(operandSlice, System.StringSplitOptions.RemoveEmptyEntries);
+            //string[] operandInput = userInput.Split(operandSlice, System.StringSplitOptions.RemoveEmptyEntries);
 
-            for (var i = 0; i < slicedInputs.Length; i++)
-            {
-                
-                                                                  
-            }
+
 
             //string userInput = Console.ReadLine();
             //char[] numInput = userInput.Split(delimiterChars);
 
-            
+
             //place holder
             return null;
         }
